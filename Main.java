34ccadd9662b7b641699c1ea176bca3732d49b4b
lucyimhoff <lucyi4@nycstudents.net>
2025-03-18 13:28:03 +0000

    textbook class started

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main{
     public static void main(String[] args){
+        Textbook bio2015 = new Textbook("Biology", 49.75, 2);
+        Textbook bio2019 = new Textbook("Biology", 39.75, 2);
+        System.out.println(bio2019.getEdition());
+        System.out.println(bio2019.getBookInfo());
     }
 }
--- Textbook.java
+++ Textbook.java
@@ -6,4 +6,14 @@ public class Textbook extends Book{
         this.edition = edition;
     }
 
+    public int getEdition(){
+        return edition;
+    }
+
+    public String getBookInfo(){
+        return super.getBookInfo() + "-" edition;
+    }
+
+    
+
 }
